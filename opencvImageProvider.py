5cd9c1134de2bd5806a12399c7c3996e808e9edf
Szczempus <46344509+Szczempus@users.noreply.github.com>
2022-01-30 14:19:19 +0100

    cos

--- opencvImageProvider.py
+++ opencvImageProvider.py
@@ -1,31 +1,43 @@
 # This Python file uses the following encoding: utf-8
-from PySide2.QtCore import QObject, QThread, QSize, Slot
-from PySide2.QtGui import QImageReader, QImage
-from PySide6.QtQml import QQmlImageProviderBase
-from PySide2.QtQuick import QQuickImageProvider, QQuickAsyncImageProvider
+from PySide2.QtCore import QSize, QByteArray
+from PySide2.QtGui import QImage, QImageReader
+from PySide2.QtQuick import QQuickImageProvider
 import cv2 as cv
 
 
-class OpencvImageProvider(QQuickAsyncImageProvider):
+class OpencvImageProvider(QQuickImageProvider):
     def __init__(self):
         # QThread.__init__(self)
-        QQuickAsyncImageProvider.__init__(self, QQuickAsyncImageProvider.Image)
-
+        super(OpencvImageProvider, self).__init__(QQuickImageProvider.Image,
+                                                  QQuickImageProvider.ForceAsynchronousImageLoading)
         self.image_file_path = None
         self.image = None
+
 
         pass
 
     def requestImage(self, path: str, size: QSize, req_size: QSize) -> QImage:
         img = self.image
         if img is None:
-            path = path.split("///", 1)
+            self.image_file_path = path.split("///", 1)
+            reader = QImageReader(path[1])
             print(path[1])
+            img = cv.imread(path[1])
 
-            # img = cv.imread(path[1])
-            # h, w, _ = img.shape
+            # Todo załadowac obraz tiff, zrobić checking itp
 
-            # qimage = QImage(img.data, w, h, QImage.Format_BGR888)
-            # img = qimage
+            # cv.imshow("Załadowałe", img)
+            # cv.waitKey(0)
+            # cv.destroyAllWindows()
 
-        return img
+            # is_success, img_buf_array = cv.imencode(".png", img)
+            # img_byte_array = img_buf_array.tobytes()
+            #
+            # bytearr = QByteArray(img_byte_array)
+            # self.image = QImage()
+            # self.image.loadFromData(bytearr, "PNG")
+
+        # return self.image
+        return reader.read()
+
+
